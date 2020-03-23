1037be48e53ee761301ae063edf3cdc4d3969c24
unknown <smazza@ucvts.org>
2020-03-22 21:07:47 -0400

    Started Insertion2

--- src/Exercises.java
+++ src/Exercises.java
@@ -274,12 +274,41 @@ public class Exercises {
           }
 
           return list;
-          
+
       }
 
   public String[] insertion(String[] list, boolean ascending) {
-    return null;
-  }
+
+    if (list == null || list.length == 0) {
+
+         return null;
+
+     }
+
+     String temp;
+
+     if (ascending) {
+
+         for (int i = 1; i < list.length; i++) {
+
+             temp = list[i];
+
+             int j = 0;
+
+             for (j = i; j > 0; j--)
+
+                 if (temp.compareTo(list[j - 1]) < 0) {
+
+                     list[j] = list[j - 1];
+
+                 } else {
+
+                     break;
+
+                 }
+                 
+              list[j] = temp;
+         }
 
   public int[] selection(int[] list, boolean ascending) {
     return null;
