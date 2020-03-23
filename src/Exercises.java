bf39c78572b3d04bb3d5df6ee393e02e59ab1a3b
unknown <smazza@ucvts.org>
2020-03-22 21:02:53 -0400

    Finished Insertion

--- src/Exercises.java
+++ src/Exercises.java
@@ -244,9 +244,38 @@ public class Exercises {
               }
           }
 
-    return null;
-    
-  }
+        } else if (!ascending) {
+
+              for (int i = 1; i < list.size(); i++) {
+
+                  int key = list.get(i);
+
+                  for (int j = i - 1; j >= 0; j--) {
+
+                      if (key > list.get(j)) {
+
+                          list.set(j + 1,list.get(j));
+
+                          if (j == 0) {
+
+                              list.set(0, key);
+
+                          }
+
+                      } else {
+
+                          list.set(j + 1, key);
+
+                          break;
+
+                      }
+                  }
+              }
+          }
+
+          return list;
+          
+      }
 
   public String[] insertion(String[] list, boolean ascending) {
     return null;
