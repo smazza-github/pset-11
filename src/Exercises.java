24650619b73fca289102e80a6ff058a081926f67
unknown <smazza@ucvts.org>
2020-03-22 20:57:29 -0400

    Started Insertion

--- src/Exercises.java
+++ src/Exercises.java
@@ -209,7 +209,43 @@ public class Exercises {
           }
 
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
+
+    if (list == null || list.size() == 0) {
+
+          return null;
+
+      }
+
+      if (ascending) {
+
+          for (int i = 1; i < list.size(); i++) {
+
+              int key = list.get(i);
+
+              for (int j = i - 1; j >= 0; j--) {
+
+                  if (key < list.get(j)) {
+
+                      list.set(j + 1,list.get(j));
+
+                      if (j == 0) {
+
+                          list.set(0, key);
+
+                      }
+
+                  } else {
+
+                      list.set(j + 1, key);
+
+                      break;
+
+                  }
+              }
+          }
+
     return null;
+    
   }
 
   public String[] insertion(String[] list, boolean ascending) {
